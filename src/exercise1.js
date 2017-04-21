8739fd82b575413715731458a822b81506e8e50a
Lucas BILLET <lucas.billet@ynov.com>
2017-04-21 09:34:16 +0200

    Exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -3,8 +3,14 @@ const chalk = require('chalk');
 console.log(chalk.cyan('\nEXERCISE 1 LOADED\n'));
 
 const zip = (input) => {
+  if(input[0]){
+    return input[0].map(function(_,i){
+            return input.map(function(array){return array[i]})
+        });
+  } else {
+    return [];
+  }
 
-  // Your future job begins here ...
 }
 
 module.exports = {
