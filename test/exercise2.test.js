efa5c998d8290ece813b4435b00bb43ca07f4457
Lucas BILLET <lucas.billet@ynov.com>
2017-04-21 10:14:26 +0200

    Exercise 2

--- test/exercise2.test.js
+++ test/exercise2.test.js
@@ -4,9 +4,7 @@ const reverseYolo = require('../src/exercise2').reverseYolo;
 describe('GIVEN function reverseYolo', function() {
 
   it('WHEN my mystery input THEN my wonderful output', function() {
-
-    // Your future job begins here ...
-    let whereIsMyMind = '???';
+    let whereIsMyMind = [['G','K','N','O','R','T'],['0','3','3'],[0,0,1,1,2,2,4,7]];
 
     expect(reverseYolo(whereIsMyMind)).to.eql([ 'T', 'R', 'O', 'N', 'K', 'G', '3', '3', '0', 7, 4, 2, 2, 1, 1, 0, 0 ]);
   });
