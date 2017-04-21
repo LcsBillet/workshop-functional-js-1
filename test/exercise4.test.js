c626821e46881442e651c8ad8bde2edd142fd0fb
Lucas BILLET <lucas.billet@ynov.com>
2017-04-21 10:49:49 +0200

    Last commit

--- test/exercise4.test.js
+++ test/exercise4.test.js
@@ -4,9 +4,6 @@ const asyncYolo = require('../src/exercise4').asyncYolo;
 describe('GIVEN function asyncYolo', function() {
 
   it('WHEN asynchronous thing is not broken THEN should return THE result', function() {
-
-    // Your future job begins here ...
-
     expect(asyncYolo()).to.eql([101, 102, 203]);
   });
 
